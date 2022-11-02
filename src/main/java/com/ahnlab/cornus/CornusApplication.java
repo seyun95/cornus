672928f0bbecc3c3a7d0a4f0106a14dc5e5f1837
jmyang <jmyang.code@gmail.com>
2022-11-02 22:11:44 +0900

    porject setting

---
+++ src/main/java/com/ahnlab/cornus/CornusApplication.java
@@ -0,0 +1,13 @@
+package com.ahnlab.cornus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CornusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahnlab/cornus/CornusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahnlab.cornus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
